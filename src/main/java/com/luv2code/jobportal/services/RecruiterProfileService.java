0c5d8b8e9340b343383d543eb6d91b9bc66dc335
luizricardogodoy <luizricardogodoy@gmail.com>
2024-08-06 15:14:41 -0300

    first commit

---
+++ src/main/java/com/luv2code/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,43 @@
+package com.luv2code.jobportal.services;
+
+import com.luv2code.jobportal.entity.RecruiterProfile;
+import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.repository.RecruiterProfileRepository;
+import com.luv2code.jobportal.repository.UsersRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterProfileRepository;
+    private final UsersRepository usersRepository;
+
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository, UsersRepository usersRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id){
+        return recruiterProfileRepository.findById(id);
+    }
+
+    public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
+        return recruiterProfileRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUser = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUser).orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+            Optional<RecruiterProfile> recruiterProfile = getOne(users.getUserId());
+            return  recruiterProfile.orElse(null);
+        }else return null;
+    }
+}
